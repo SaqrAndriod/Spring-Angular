ac61bc51cc34e700a6553329283a658201710628
AhmedHassanIbrahim <a.ibrahim@everteam-gs.com>
2019-12-28 17:54:49 +0200

    spring angular

---
+++ Spring-Angular/Angular8-SpringBoot-CRUD/employess-server/src/main/java/com/employees/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.employees.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.employees.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
